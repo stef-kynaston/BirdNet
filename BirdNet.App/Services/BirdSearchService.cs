efc0009afe9a1333c785858d9ee0c36c38978695
stef-kynaston <200280563+stef-kynaston@users.noreply.github.com>
2025-03-22 21:47:59 +0000

    Include higher taxa for scientific name search

--- BirdNet.App/Services/BirdSearchService.cs
+++ BirdNet.App/Services/BirdSearchService.cs
@@ -51,6 +51,9 @@ public class BirdSearchService
 
         return await _dbContext.Species
             .Where(species => species.ScientificName.ToLower().Contains(searchQuery))
+            .Include(species => species.Genus)
+            .ThenInclude(genus => genus.Family)
+            .ThenInclude(family => family.Order)
             .ToListAsync();
     }
 }
