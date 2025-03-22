c355a10940cea66a24b6a1df221267a68bd36c3a
stef-kynaston <200280563+stef-kynaston@users.noreply.github.com>
2025-03-22 22:31:56 +0000

    Add Converters.xaml

--- BirdNet.App/Converters/LineageConverter.cs
+++ BirdNet.App/Converters/LineageConverter.cs
@@ -2,7 +2,7 @@ using System.Globalization;
 using System.Windows.Data;
 using BirdNet.Data.Entities;
 
-namespace BirdNet.Utilities;
+namespace BirdNet.Converters;
 
 public class LineageConverter : IValueConverter
 {
